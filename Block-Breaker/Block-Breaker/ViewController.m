c0856765d90912f624ca2a87edc27dc9b29a53a3
i8023eva <1009175863@qq.com>
2015-10-25 17:00:21 +0800

    小球运动

--- Block-Breaker/Block-Breaker/ViewController.m
+++ Block-Breaker/Block-Breaker/ViewController.m
@@ -7,12 +7,41 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGesture;
+
+@property (weak, nonatomic) IBOutlet UIImageView *ballImageView;
+// 初始位置
+@property (nonatomic, assign) CGPoint ballCenter;
+//游戏时钟
+@property (nonatomic, strong) CADisplayLink *gameTimer;
+//速度
+@property (nonatomic, assign) CGPoint ballVelocity;
 
 @end
 
 @implementation ViewController
+
+-(void) step{
+    NSLog(@"step");
+    //更新小球位置
+    self.ballImageView.center = CGPointMake(self.ballImageView.centerX + self.ballVelocity.x, self.ballImageView.centerY + self.ballVelocity.y);
+}
+
+- (IBAction)tapScreen:(id)sender {
+    NSLog(@"error ---%s ---%d", __func__, __LINE__);
+    //停止手势, 防止速度累加
+    self.tapGesture.enabled = NO;
+    /**
+     *  设置初始速度
+     */
+    self.ballVelocity = CGPointMake(0.0, -5.0);
+    
+    self.gameTimer = [CADisplayLink displayLinkWithTarget:self selector:@selector(step)];
+    [self.gameTimer addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+}
 
 -(BOOL)prefersStatusBarHidden
 {
@@ -22,6 +51,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //记录初始位置
+    self.ballCenter = self.ballImageView.center;
 }
 
 - (void)didReceiveMemoryWarning {
